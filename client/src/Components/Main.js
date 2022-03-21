4aa4bc69f4821db591eb75496cf4fa978aaae74f
ccarver80 <ckc_80@outlook.com>
2022-03-21 10:20:19 -0600

    working on side bar

--- client/src/Components/Main.js
+++ client/src/Components/Main.js
@@ -1,10 +1,40 @@
 import React from "react";
 import Header from "./Header";
 
+import dropDown from '../Style/Imgs/dropDownSideBar.png'
+
 function Main() {
     return (
+        <>
         <Header />
+        {/* Main Container */}
+        <div className="flex mx-auto w-[1200px]  h-[2108.59px] mt-6">
+                {/* ==========Side Bar =========== */}
+                <div className="flex flex-col w-[324px] h-[1312.04px]">
+                    <div className="flex flex-row shadow-sideBar w-[324px] h-[432px] rounded-tr-[7px] rounded-bl-[3px] pt-[30px] pb-[30px] pl-[27px] pr-[27px] ">
+                        <div>
 
+                            <div className="flex flex-start mt-0 mr-[10px] w-[240px] h-[22px]">
+                                <h1 className="font-Inter text-lg ">7-Day Fast Start</h1>
+                                <img className="flex my-auto ml-auto  w-3 h-[6px] " src={dropDown} />
+                            </div>
+                            
+                            <div className="flex mt-4">
+                                <div className=" w-full rounded-xl bg-progress-background">
+                                    <div className=" w-[25%] h-2  bg-progress-green rounded-xl "> </div>   
+                                </div>
+                               
+                            <label htmlFor=""
+                            </div>
+                            
+                            
+                            
+                        </div>
+                    </div>
+                </div>
+
+        </div>
+        </>
     )
 }
 
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -7,8 +7,8 @@ module.exports = {
     extend: {
     fontFamily: {
         'Roboto': ['"Roboto"', 'sans-serif'],
-        'Poppins': ['"Poppins"', 'sans-serif']
-        
+        'Poppins': ['"Poppins"', 'sans-serif'],
+        'Inter': ['"Inter"', 'sans-serif']
       }, 
       colors: {
         'light-blue': '#2478CC',
@@ -17,6 +17,11 @@ module.exports = {
         'goldenrod': '#F4AF32',
         'blue-border': '#2478CC', 
         'golden-button': '#FFC700',
+        'progress-background': '#EFF6FA',
+        'progress-green': '#0C9C00',
+      },
+      boxShadow: {
+        'sideBar': '0px 0px 1px rgba(12, 26, 75, 0.24), 0px 3px 8px -1px rgba(50, 50, 71, 0.05)',
       }
     },
   },
